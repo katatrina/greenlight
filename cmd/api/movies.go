b59b21fa7ee4a69a872b19d6528428471ab95db8
chauvinhphuoc <cvphuoc2014@gmail.com>
2023-11-19 10:22:38 +0700

    custom runtime field response

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -11,12 +11,16 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 }
 
 type showMovieResponse struct {
-	ID      int64    `json:"id"`
-	Title   string   `json:"title"`
-	Year    int32    `json:"year,omitempty"`
-	Runtime int32    `json:"runtime,omitempty"`
-	Genres  []string `json:"genres,omitempty"`
-	Version int32    `json:"version"`
+	ID    int64  `json:"id"`
+	Title string `json:"title"`
+	Year  int32  `json:"year,omitempty"`
+	// Use the Runtime type instead of int32. Note that the omitempty directive will
+	// still work on this: if the Runtime field has the underlying value 0, then it will
+	// be considered empty and omitted -- and the MarshalJSON() method we just made
+	// won't be called at all.
+	Runtime db.Runtime `json:"runtime,omitempty"`
+	Genres  []string   `json:"genres,omitempty"`
+	Version int32      `json:"version"`
 }
 
 func newShowMovieResponse(movie db.Movie) *showMovieResponse {
@@ -24,7 +28,7 @@ func newShowMovieResponse(movie db.Movie) *showMovieResponse {
 		ID:      movie.ID,
 		Title:   movie.Title,
 		Year:    movie.Year,
-		Runtime: movie.Runtime,
+		Runtime: db.Runtime(movie.Runtime),
 		Genres:  movie.Genres,
 		Version: movie.Version,
 	}
