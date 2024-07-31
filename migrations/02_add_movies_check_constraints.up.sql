dcf39dd3d4263348012ead959410f538ca39949d
katatrina <cvphuoc2014@gmail.com>
2024-07-31 14:48:10 +0700

    init database schema

---
+++ migrations/02_add_movies_check_constraints.up.sql
@@ -0,0 +1,12 @@
+ALTER TABLE movies
+ADD CONSTRAINT movies_runtime_check CHECK (runtime >= 1 AND runtime <= 300);
+
+ALTER TABLE movies
+ADD CONSTRAINT movies_year_check CHECK (
+        year BETWEEN 1888 AND date_part('year', now())
+    );
+
+ALTER TABLE movies
+ADD CONSTRAINT genres_length_check CHECK (
+        array_length(genres, 1) BETWEEN 1 AND 5
+    );
